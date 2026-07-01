f411d27ea3611e900566fde8d2e717e8c66ae91b
Nikolenko.Sergei <Nikolenko.Sergei@icloud.com>
2026-07-01 20:22:59 +0400

    Update Burrete cask to 1.0.14

--- Casks/burrete.rb
+++ Casks/burrete.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "burrete" do
-  version "1.0.13"
-  sha256 "066919d1769b4e54bbd321100883ef882e519e289ca8874ff23a0bff65013625"
+  version "1.0.14"
+  sha256 "661d51caa308ae2f580d77ee8405372cc96b977403aa128e4cd63e428eb70a9e"
 
   url "https://github.com/SergeiNikolenko/Burrete/releases/download/v#{version}/Burrete-#{version}.zip"
   name "Burrete"
